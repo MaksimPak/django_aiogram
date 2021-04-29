fd13271dba880627db70b67e13afc38a0f9a320f
raging potato <potato@Maks-PC.local>
2021-04-29 15:52:00 +0500

    models

--- src/app/settings.py
+++ src/app/settings.py
@@ -135,3 +135,4 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads')
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'dashboard.User'
--- src/dashboard/models.py
+++ src/dashboard/models.py
@@ -1,3 +1,69 @@
+import uuid
+
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+
+class User(AbstractUser):
+    pass
+
+
+class Student(models.Model):
+    class ApplicationType(models.TextChoices):
+        web = '1', 'Web',
+        telegram = '2', 'Telegram'
+
+    first_name = models.CharField(max_length=50, verbose_name='Имя')
+    last_name = models.CharField(max_length=50, verbose_name='Фамилия')
+    tg_id = models.BigIntegerField(verbose_name='Telegram ID', blank=True, null=True)
+    phone = models.CharField(max_length=20)
+    application_type = models.CharField(max_length=20, choices=ApplicationType.choices, default=ApplicationType.web)
+    register_link = models.CharField(max_length=200, blank=True, null=True, verbose_name='Ссылка на регистрацию')
+    courses = models.ManyToManyField('Course', through='StudentCourse')
+    lessons = models.ManyToManyField('Lesson')
+
+
+class Stream(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Course(models.Model):
+    class CategoryType(models.TextChoices):
+        game_dev = '1', 'Game Development'
+        web = '2', 'Web Development',
+
+    class DifficultyType(models.TextChoices):
+        easy = '1', 'Beginner',
+        medium = '2', 'Intermediate',
+        hard = '3', 'Advanced'
+
+    name = models.CharField(max_length=50)
+    info = models.TextField(blank=True, null=True)
+    category = models.CharField(max_length=20, choices=CategoryType.choices)
+    difficulty = models.CharField(max_length=20, choices=DifficultyType.choices)
+    price = models.BigIntegerField()
+
+
+class Lesson(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название урока')
+    info = models.TextField(blank=True, null=True)
+
+
+class LessonUrl(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE, primary_key=True)
+    hash = models.UUIDField(default=uuid.uuid4)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE, verbose_name='Урок')
+
+
+class StudentCourse(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    course = models.OneToOneField(Course, on_delete=models.CASCADE)
+    stream = models.ForeignKey(Stream, on_delete=models.CASCADE)
+
+
+class StudentLesson(models.Model):
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    lesson = models.OneToOneField(Lesson, on_delete=models.CASCADE)
+    has_homework = models.BooleanField(verbose_name='Есть домашнее задание', default=False)
