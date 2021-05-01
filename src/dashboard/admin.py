6115e51a051aad7a512b9c588958c16ca66ebf0c
raging potato <potato@Maks-PC.local>
2021-05-01 10:43:23 +0500

    tg broadcast

--- src/dashboard/admin.py
+++ src/dashboard/admin.py
@@ -1,5 +1,11 @@
+import os
+
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+import requests
+
 from dashboard import models
 
 
@@ -17,6 +23,19 @@ class ClientAdmin(admin.ModelAdmin):
     list_display = ('id', '__str__', 'phone')
     list_display_links = ('__str__',)
     inlines = (CourseList,)
+    actions = ('send_message',)
+
+    def send_message(self, request, qs):
+        clients = models.Client.objects.all()
+
+        if 'send' in request.POST:
+            for client in clients:
+                url = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage?chat_id={client.tg_id}&text={request.POST['message']}"
+                requests.get(url)
+            return HttpResponseRedirect(request.get_full_path())
+
+        return render(request, 'dashboard/send_intermediate.html', context={'clients': clients})
+    send_message.short_description = 'Массовая рассылка'
 
 
 admin.site.register(models.User, UserAdmin)
