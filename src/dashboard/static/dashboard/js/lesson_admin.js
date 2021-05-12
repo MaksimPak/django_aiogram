f6924800a9d788543203828f2c6e0b960dbddab8
raging potato <potato@Maks-PC.local>
2021-05-12 09:51:41 +0500

    lesson admin js fix

--- src/dashboard/static/dashboard/js/lesson_admin.js
+++ src/dashboard/static/dashboard/js/lesson_admin.js
@@ -1,5 +1,9 @@
 window.addEventListener('load', function() {
     (function($) {
+        if ($('.field-has_homework input:checkbox:checked').length === 0)
+        {
+            $('.field-homework_desc').attr('hidden', true)
+        }
         $('#id_has_homework').change(function() {
             if (this.checked) {
                 $('.field-homework_desc').show()
