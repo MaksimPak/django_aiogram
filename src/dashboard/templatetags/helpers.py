43454385bc8b1aa33ddb2fed9d1f9d7d17ac8b56
raging potato <potato@Maks-PC.local>
2021-05-13 18:03:53 +0500

    unique names in progress table

--- src/dashboard/templatetags/helpers.py
+++ src/dashboard/templatetags/helpers.py
@@ -1,8 +1,20 @@
 from django import template
 
+from dashboard.models import Student
+
 register = template.Library()
 
 
-@register.filter(name='homework_count')
-def homework_count(qs):
-    return qs.studentlesson_set.filter(homework_sent__isnull=False).count()
+@register.filter(name='students')
+def students(qs):
+    student_lesson = qs.studentlesson_set.all().values('student').distinct()
+    clients = [Student.objects.get(pk=x['student']) for x in student_lesson]
+    return clients
+
+
+@register.filter(name='students_hw_submitted')
+def students_hw_submitted(qs):
+    student_lesson = qs.studentlesson_set.filter(homework_sent__isnull=False).values('student').distinct()
+    clients = [Student.objects.get(pk=x['student']) for x in student_lesson]
+    return clients
+
