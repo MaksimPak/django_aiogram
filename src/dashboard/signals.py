7d74cda095c08af30b5a72e03b3562042e42997b
raging potato <potato@Maks-PC.local>
2021-05-17 11:52:28 +0500

    add message remade

--- src/dashboard/signals.py
+++ src/dashboard/signals.py
@@ -2,30 +2,39 @@ import json
 import os
 
 import requests
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_init
 from django.dispatch import receiver
 
 from dashboard.models import StudentCourse, Client, Lead, Course, Student, random_int
 
 
-@receiver(post_save, sender=StudentCourse)
+@receiver(post_init, sender=Course)
+def set_is_started_copy(sender, instance, *args, **kwargs):
+    instance._is_started = instance.is_started
+
+
+@receiver(post_save, sender=Course)
 def send_course_add_message(sender, instance, created, **kwargs):
-    if created:
-        kb = {
-            'inline_keyboard': [
-                [{
-                    'text': 'Начать Курс',
-                    'callback_data': f'get_course|{instance.course.id}'
-                }],
-            ]
-        }
-        d = {
-            'chat_id': instance.student.tg_id,
-            'text': instance.course.add_message,
-            'reply_markup': json.dumps(kb)
-        }
-        url = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage"
-        requests.post(url, data=d)
+
+    if instance._is_started is not True:
+        for student in instance.student_set.all():
+            kb = {
+                'inline_keyboard': [
+                    [{
+                        'text': 'Начать Курс',
+                        'callback_data': f'get_course|{instance.id}'
+                    }],
+                ]
+            }
+            d = {
+                'chat_id': student.tg_id,
+                'text': instance.add_message,
+                'reply_markup': json.dumps(kb)
+            }
+            url = f"https://api.telegram.org/bot{os.getenv('BOT_TOKEN')}/sendMessage"
+            requests.post(url, data=d)
+
+        instance._is_started = instance.is_started
 
 
 @receiver(post_save, sender=Client)
