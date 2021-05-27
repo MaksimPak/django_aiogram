8ad6307d1fc41ff4ad056b7f508c7f443bc1f49e
raging potato <potato@Maks-PC.local>
2021-05-27 14:08:13 +0500

    course end message

--- src/dashboard/signals.py
+++ src/dashboard/signals.py
@@ -20,21 +20,34 @@ def set_is_started_copy(sender, instance, *args, **kwargs):
     Setting local variable _is_started to track if course was started during editing of the course
     """
     instance._is_started = instance.is_started
+    instance._is_finished = instance.is_finished
 
 
 @receiver(post_save, sender=Course)
 def send_course_add_message(sender, instance, created, **kwargs):
     """
-    Check if is_started variable has been set to True after editing. If yes, send a course message to all students
+    Check if is_started variable has been set to True after editing. If yes, send start message to all students
     """
     if instance._is_started is not True:
-        course_id = instance.id
-        kb = json.dumps({'inline_keyboard': [[{'text': 'Начать Курс', 'callback_data': f'get_course|{course_id}'}]]})
+        kb = json.dumps({'inline_keyboard': [[{'text': 'Начать Курс', 'callback_data': f'get_course|{instance.id}'}]]})
         people = instance.student_set.all()
-        Telegram.send_messages(people, instance.add_message, kb)
+        Telegram.send_messages(people, instance.start_message, kb)
 
         instance._is_started = instance.is_started
         Course.objects.filter(pk=instance.id).update(date_started=datetime.datetime.now())
+
+
+@receiver(post_save, sender=Course)
+def send_course_finish_message(sender, instance, created, **kwargs):
+    """
+    Check if is_finished variable has been set to True after editing. If yes, send end message to all students
+    """
+    if instance._is_finished is not True:
+        people = instance.student_set.all()
+        Telegram.send_messages(people, instance.end_message)
+
+        instance._is_finished = instance.is_finished
+        Course.objects.filter(pk=instance.id).update(date_finished=datetime.datetime.now())
 
 
 @receiver(post_save, sender=Lead)
