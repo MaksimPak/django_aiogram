c76e97912f9fc54ba8beb016c70f55b50ab00a43
raging potato <potato@Maks-PC.local>
2021-06-07 09:29:43 +0500

    sentry bot

--- src/bot/config.py
+++ src/bot/config.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 dotenv_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), '.env')
 load_dotenv(dotenv_path)
 
-BOT_TOKEN = os.environ.get('BOT_TOKEN')
 DOMAIN = os.environ.get('DOMAIN', 'http://127.0.0.1:8000')
+APP_ENVIRONMENT = os.environ.get('APP_ENV', 'development')
+
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+WEBHOOK_PATH = f'/{BOT_TOKEN}/webhook'
+WEBHOOK_URL = f'{DOMAIN}{WEBHOOK_PATH}'
+BOT_PUBLIC_PORT = os.environ.get('BOT_PUBLIC_PORT', 8080)
 
 DB_URL = os.environ.get('DB_URL')
 REDIS_HOST = os.environ.get('REDIS_HOST', 'redis')
 REDIS_PORT = os.environ.get('REDIS_PORT', 6379)
-
-WEBHOOK_PATH = f'/{BOT_TOKEN}/webhook'
-WEBHOOK_URL = f'{DOMAIN}{WEBHOOK_PATH}'
-BOT_PUBLIC_PORT = os.environ.get('BOT_PUBLIC_PORT', 8080)
 
 CHAT_ID = os.environ.get('CHAT_ID')
--- src/bot/misc.py
+++ src/bot/misc.py
@@ -2,6 +2,7 @@ from aiogram import Bot, Dispatcher
 from aiogram.contrib.fsm_storage.redis import RedisStorage2
 from jinja2 import Environment, PackageLoader, select_autoescape
 from loguru import logger
+import sentry_sdk
 
 
 from bot import config
@@ -24,4 +25,14 @@ def setup():
 
     # noinspection PyUnresolvedReferences
     import bot.handlers
+
+    if config.APP_ENVIRONMENT != 'development':
+        sentry_sdk.init(
+            "https://8c853fb11a534fe692ac7b6db063b6fb@o437434.ingest.sentry.io/5803464",
+
+            # Set traces_sample_rate to 1.0 to capture 100%
+            # of transactions for performance monitoring.
+            # We recommend adjusting this value in production.
+            traces_sample_rate=1.0
+        )
 
