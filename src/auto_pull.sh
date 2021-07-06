009fa269f6a231f329da4ae95facfb2ea1bb8133
potato <maxim.park1235@gmail.com>
2021-07-06 09:51:11 +0500

    cicd do not install requirements

--- src/auto_pull.sh
+++ src/auto_pull.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 source ../../venv/bin/activate
 git pull
-pip install -r requirements.txt
 python manage.py migrate
 supervisorctl restart supersonic_admin
 supervisorctl restart supersonic_bot
