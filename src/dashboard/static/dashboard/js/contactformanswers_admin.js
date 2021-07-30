6d9400e3d89ae2a6afd16a0b086bdc9e2a3e5ae5
potato <maxim.park1235@gmail.com>
2021-07-30 14:34:53 +0500

    js file

---
+++ src/dashboard/static/dashboard/js/contactformanswers_admin.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function() {
+    (function($) {
+        $('#changelist-filter-clear').remove()
+        $('#changelist-filter li:first-child').remove()
+    })(django.jQuery);
+});
