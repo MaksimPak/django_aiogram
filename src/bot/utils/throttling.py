ffe4553cca90d02f1f0b90fd4c091bf4f9162cb6
potato <maxim.park1235@gmail.com>
2021-08-03 17:41:47 +0500

    throttling answer cb if type correct

--- src/bot/utils/throttling.py
+++ src/bot/utils/throttling.py
@@ -6,6 +6,8 @@ from aiogram import types
 
 async def throttled(*args, **kwargs):
     response: Union[types.Message, types.CallbackQuery] = args[0]
+    if type(response) == types.CallbackQuery:
+        await response.answer()
 
     message = response if type(response) == types.Message else response.message
     logger.info(f'User: {message.from_user.id} caused throttling.\nMessage:{message.text}')
