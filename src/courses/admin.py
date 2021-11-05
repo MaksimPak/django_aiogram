e1f564c48c558255c9b799a2b6bb987c370683ab
potato <maxim.park1235@gmail.com>
2021-11-05 13:46:08 +0500

    course admin. progress bar reimplemented

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,11 +1,13 @@
-from django import forms
 from django.contrib import admin, messages
-
+from django.shortcuts import render
 from django.template.loader import render_to_string
+from django.urls import reverse
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
+from broadcast.forms import BroadcastForm
 from courses import models
-from courses.filters import FilterByCourse
+from courses.filters import FilterByCourse, StatusFilter
 from courses.forms import CourseForm
 
 
@@ -60,7 +62,7 @@ class CourseAdmin(admin.ModelAdmin):
     date_hierarchy = 'created_at'
     form = CourseForm
     change_form_template = 'courses/admin/change_form.html'
-    actions = ['duplicate']
+    actions = ('duplicate',)
 
     @admin.display(description='Дублировать (Максимум 3)')
     def duplicate(self, request, courses):
@@ -92,9 +94,31 @@ class CourseAdmin(admin.ModelAdmin):
 
 @admin.register(models.StudentLesson)
 class StudentProgress(admin.ModelAdmin):
-    list_display = ('lesson_name', 'lesson_received', 'lesson_watched',
-                    'hw_submitted',)
-    list_filter = (FilterByCourse,)
+    list_filter = (FilterByCourse, StatusFilter,)
+
+    def get_queryset(self, request):
+        if not request.GET:
+            return models.StudentLesson.objects.none()
+        return super(StudentProgress, self).get_queryset(request)
+
+    def get_model_perms(self, request):
+        """
+        Return empty perms dict thus hiding the model from admin index.
+        """
+        return {}
+
+    def get_list_display(self, request):
+        if request.GET.get('course_id'):
+            return ('lesson_name', 'lesson_received', 'lesson_watched',
+                    'hw_submitted', 'details',)
+        else:
+            return ('student',)
+
+    def get_actions(self, request):
+        actions = super(StudentProgress, self).get_actions(request)
+        if request.GET.get('lesson__id__exact'):
+            self.actions.append(self.send_message)
+        return actions
 
     @admin.display(description='Урок')
     def lesson_name(self, instance):
@@ -104,8 +128,9 @@ class StudentProgress(admin.ModelAdmin):
     def lesson_received(self, instance):
         course_id = instance.lesson.course_id
         count = models.StudentLesson.objects.filter(
-            lesson__course__pk=course_id,
-            date_watched__isnull=False,
+            lesson__course_id=course_id,
+            lesson=instance.lesson,
+            date_sent__isnull=False,
         ).count()
         return count
 
@@ -113,7 +138,8 @@ class StudentProgress(admin.ModelAdmin):
     def lesson_watched(self, instance):
         course_id = instance.lesson.course_id
         count = models.StudentLesson.objects.filter(
-            lesson__course__pk=course_id,
+            lesson__course_id=course_id,
+            lesson=instance.lesson,
             date_watched__isnull=False,
         ).count()
         return count
@@ -122,39 +148,31 @@ class StudentProgress(admin.ModelAdmin):
     def hw_submitted(self, instance):
         course_id = instance.lesson.course_id
         count = models.StudentLesson.objects.filter(
-            lesson__course__pk=course_id,
+            lesson__course_id=course_id,
+            lesson=instance.lesson,
             homework_sent__isnull=False,
             lesson__homework_desc__isnull=False
         ).count()
         return count if count else 'Нет домашки'
 
-    def get_queryset(self, request):
-        if request.GET.get('course_id'):
-            qs = super(StudentProgress, self).get_queryset(request)
-            qs = qs.filter(lesson__course_id=request.GET['course_id'])
-            return qs
-        return models.StudentLesson.objects.none()
+    @admin.display(description='Детали')
+    def details(self, instance):
+        model = instance._meta.model_name
+        app = instance._meta.app_label
+        changeform_url = reverse(
+            f'admin:{app}_{model}_changelist'
+        )
+        changeform_url += f'?lesson__id__exact={instance.lesson.id}'
+        return mark_safe(f'<a href="{changeform_url}">Посмотреть</a>')
 
-    def get_model_perms(self, request):
-        """
-        Return empty perms dict thus hiding the model from admin index.
-        """
-        return {}
-
-# @admin.register(models.Lesson)
-# class LessonAdmin(admin.ModelAdmin):
-#     list_display = ('id', '__str__', 'lesson_info', 'video', 'course')
-#     list_display_links = ('__str__',)
-#     list_per_page = 20
-#     readonly_fields = ('date_sent',)
-#     search_fields = ('id', 'title', 'course')
-#     list_filter = ('course',)
-#     ordering = ('id',)
-#     date_hierarchy = 'created_at'
-#
-#     class Media:
-#         js = (
-#             'dashboard/js/lesson_admin.js',
-#         )
-#
-#
+    @staticmethod
+    @admin.display(description='Массовая рассылка')
+    def send_message(modeladmin, request, queryset):
+        contacts = [x.student.contact for x in queryset]
+        form = BroadcastForm(initial={'_selected_action': [contact.id for contact in contacts]})
+        context = {
+            'entities': contacts,
+            'form': form,
+            'referer': request.META['HTTP_REFERER'],
+        }
+        return render(request, "broadcast/send.html", context=context)
--- src/courses/filters.py
+++ src/courses/filters.py
@@ -1,15 +1,43 @@
-from datetime import date
-
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
 
 
 class FilterByCourse(admin.SimpleListFilter):
-    title = 'Список курсов'
+    title = ''
     parameter_name = 'course_id'
+    template = "courses/admin/empty_filter.html"
 
     def lookups(self, request, model_admin):
         return (request.GET.get(self.parameter_name), ''),
 
     def queryset(self, request, queryset):
-        return queryset
+        if self.value():
+            course_id = self.value()
+            qs = queryset.filter(lesson__course_id=course_id)
+            return qs
+
+
+class StatusFilter(admin.SimpleListFilter):
+    title = 'Фильтр уроков'
+    parameter_name = 'status'
+
+    def lookups(self, request, model_admin):
+        return (
+            ('sent', 'Получили урок'),
+            ('watched', 'Посмотрели урок'),
+            ('submitted', 'Сдали домашку'),
+        )
+
+    def queryset(self, request, queryset):
+        lesson_id = request.GET.get('lesson__id__exact')
+        if self.value() == 'sent':
+            return queryset.filter(date_sent__isnull=False,
+                                   lesson_id=lesson_id)
+
+        if self.value() == 'watched':
+            return queryset.filter(date_watched__isnull=False,
+                                   lesson_id=lesson_id)
+
+        if self.value() == 'submitted':
+            return queryset.filter(homework_sent__isnull=False,
+                                   lesson_id=lesson_id)
+
