eaf384f2325aabe051c2f624f5ea6d639e484b5c
potato <maxim.park1235@gmail.com>
2021-11-10 17:02:36 +0500

    minor

--- src/bot/utils/filters.py
+++ src/bot/utils/filters.py
@@ -14,11 +14,12 @@ class UnknownContact(Filter):
     @create_session
     async def check(self, message: types.Message, session):
         contact = await repo.ContactRepository.get('tg_id', message.from_user.id, session)
-        await repo.ContactRepository.edit(contact, {
-            'first_name': message.from_user.first_name,
-            'last_name': message.from_user.last_name,
-            'blocked_bot': False
-        }, session)
+        if contact:
+            await repo.ContactRepository.edit(contact, {
+                'first_name': message.from_user.first_name,
+                'last_name': message.from_user.last_name,
+                'blocked_bot': False
+            }, session)
         return not contact
 
 
