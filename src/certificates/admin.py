3033a9709c2c3f581fe54a80eb027fa959b8dd82
Maks <potato@Maks-MacBook.local>
2022-01-05 14:21:03 +0500

    delete action from certs

--- src/certificates/admin.py
+++ src/certificates/admin.py
@@ -21,24 +21,8 @@ class CertificateAdmin(admin.ModelAdmin):
     list_display = ('id', 'student', 'course')
     readonly_fields = ('generated_cert',)
     list_filter = ('template__course',)
-    actions = ('send_cert',)
     list_per_page = 10
 
     @admin.display(description='Курс')
     def course(self, cert):
         return cert.template.course
-
-    @admin.display(description='Отправить сертификаты')
-    def send_cert(self, request, certs):
-        url = os.environ.get('DOMAIN') + '/broadcast/send/'
-        referer = os.environ.get('DOMAIN') + request.get_full_path()
-        for cert in certs:
-            headers = {'X-CSRFToken': get_token(request), 'Referer': referer}
-            data = {
-                '_selected_action': cert.student.id,
-                'text': f'Сертификат по курсу {cert.template.course}'
-            }
-            requests.post(url, data=data, headers=headers,
-                          files={'image': cert.generated_cert.read()}, cookies=request.COOKIES)
-
-
